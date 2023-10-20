bea9664695cdcd1bb4eed44f8107690341738ec5
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-10-20 23:13:44 +0200

    Finsihed QR js

--- QR-Code/script.js
+++ QR-Code/script.js
@@ -21,6 +21,38 @@ function generate(input){
     qr_code.style = "";
 
     let qr_code = new QRCpde(qr_code, {
-        text: '$'
-    })
+        text: '${input.value}',
+        width: 180,
+        height: 180,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.correctLevel.H
+    });
+
+    let download = document.createElement("button");
+    qr_code.appendChild(download);
+
+    let link = document.createElement("a");
+    link.setAttribute("download", "qr_code.png");
+    link.innerHTML = `Download`;
+
+    download.appendChild(link);
+
+    let qr_img = document.querySelector(".qr-code img");
+    let qr_canvas = document.querySelector("canvas");
+
+    if (qr_img.getAttribute("src") === null){
+        setTimeout(() => {
+            link.setAttribute("href", `${qr_canvas.toDataURL()}`);
+        }, 300);
+    }
+    else {
+        setTimeout(() => {
+            link.setAttribute("href", `${qr_img.getAttribute("src")}`);
+        }, 300);
+    }
 }
+
+generate({
+    value: "https://codepen.io/coding_dev"
+});
